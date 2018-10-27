6f30419f91b49c4e6c27dffbe85e9d844c56e798
Michael Himing <mhiming@gmail.com>
2018-10-27 18:36:35 +1100

    Trying out hmac verification

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
+import hmac
+import json
+import logging
 import os
 
 import requests
+from flask import abort
 
+webhook_secret = bytes(os.environ['WEBHOOK_SECRET'], 'utf-8')
 public_forward_url = os.environ.get('PUBLIC_FORWARD_URL')
 private_forward_url = os.environ.get('PRIVATE_FORWARD_URL')
 
 
 def github_hook(request):
-    body = request.get_json()
+    if request.method != 'POST':
+        return abort(405)
+
+    signature = request.headers.get('X-Hub-Signature')
+    if signature is None:
+        return abort(403)
+
+    body = request.get_data()
+    digest = hmac.new(webhook_secret, body, 'sha1').hexdigest()
+
+    logging.info(repr(body))  # debug
+    logging.info(digest, signature)  # debug
+
+    if not hmac.compare_digest('sha1=' + digest, signature):
+        return abort(403)
+
+    json_body = json.loads(body.decode('utf-8'))
 
     github_headers = {
         header: value
@@ -15,10 +36,10 @@ def github_hook(request):
         if header.lower().startswith('x-github')
     }
 
-    if private_forward_url and body['repository']['private'] is False:
-        requests.post(public_forward_url, json=body, headers=github_headers)
+    if private_forward_url and json_body['repository']['private'] is False:
+        requests.post(public_forward_url, json=json_body, headers=github_headers)
 
     if public_forward_url:
-        requests.post(private_forward_url, json=body, headers=github_headers)
+        requests.post(private_forward_url, json=json_body, headers=github_headers)
 
     return 'OK'
