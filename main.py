757b3285b186d88af9ff26e0ca72a78f34371032
Michael Himing <mhiming@gmail.com>
2018-10-27 17:39:41 +1100

    Init

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+
+import requests
+
+public_forward_url = os.environ.get('PUBLIC_FORWARD_URL')
+private_forward_url = os.environ.get('PRIVATE_FORWARD_URL')
+
+
+def hello_world(request):
+    body = request.get_json()
+
+    github_headers = {
+        header: value
+        for header, value in request.headers.items()
+        if header.lower().startswith('x-github')
+    }
+
+    if private_forward_url and body['repository']['private'] is False:
+        requests.post(public_forward_url, json=body, headers=github_headers)
+
+    if public_forward_url:
+        requests.post(private_forward_url, json=body, headers=github_headers)
+
+    return 'OK'
